9946fb7d86d8b9989401819a903f55fb7a590b3d
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-08-15 20:26:26 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 console.log('PORT', process.env.PORT);
 console.log('URL:', process.env.MONGO_URL)
+
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log('MONGODB connected successfully');
+        const PORT = process.env.PORT ?? 3001;
+    })
+    .catch((err) => {
+        console.log("Error on connection MongoDB ", err)
+    })

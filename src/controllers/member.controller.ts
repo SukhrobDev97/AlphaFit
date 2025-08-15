63ac4d8a5412565d2dee276c55c1215e1cf2ade1
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-08-15 23:15:26 +0900

    feat: organize spa & bssr business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,34 +1,47 @@
 import { Request, Response } from 'express';
 import { T } from '../libs/types/common';
-
+import MemberService from '../models/Member.service';
+import {LoginInput, Member, MemberInput } from '../libs/types/member';
+import Errors from '../libs/Errors';
 
 const memberController: T = {};
-memberController.goHome = (req: Request, res: Response) => {
-    try {
-        res.send('Home page')
-    }
-    catch (err) {
-        console.log('Error, gohome', err)
-    }
-};
 
-memberController.getLogin = (req: Request, res: Response) => {
-    try {
-        res.send('Login page')
-    }
-    catch (err) {
-        console.log('Error, login', err)
-    }
-};
 
-memberController.getSignup = (req: Request, res: Response) => {
+const  memberService = new MemberService();
+
+memberController.signup = async (req: Request, res: Response) => {
     try {
-        res.send('Signup page')
+        console.log("signup")
+        console.log('body:', req.body)
+
+        const input: MemberInput = req.body,
+        result: Member = await memberService.signup(input);
+        res.json({member: result});
     }
     catch (err) {
-        console.log('Error, signup', err)
+        console.log('Error, signup', err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard)
     }
 }
+
+
+
+memberController.login = async (req: Request, res: Response) => {
+    try {
+        console.log("login");
+        const input: LoginInput = req.body,
+        result = await memberService.login(input);
+
+        res.json({member: result});
+    }
+    catch (err) {
+        console.log('Error, login', err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard)
+    }
+}
+
 
 export default memberController;
 

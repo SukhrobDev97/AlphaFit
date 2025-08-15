eea02a49348a957e01cc708fbdda9f48157750de
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-08-15 20:52:20 +0900

    feat: member service model integration

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,12 @@ mongoose.connect(process.env.MONGO_URL as string, {})
     .then((data) => {
         console.log('MONGODB connected successfully');
         const PORT = process.env.PORT ?? 3001;
+        app.listen(PORT, function () {
+            console.info(`The server is running on port: ${PORT}`);
+            console.info(`Admin project on http://localhost:${PORT}/admin \n`)
+        })
     })
     .catch((err) => {
         console.log("Error on connection MongoDB ", err)
+
     })

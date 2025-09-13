2110306c36eb381f1d38979bccb823caba98b5a0
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-09-14 00:57:54 +0900

    fix: developed member-image logic

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,7 +1,34 @@
 console.log("Signup frontend javascript file");
 
 
-$(function () { });
+
+$(function () {
+    const fileTarget = $(".file-box .upload-hidden");
+    let filename;
+
+    fileTarget.on("change", function () {
+        if (window.FileReader) {
+            const uploadFile = $(this)[0].files[0];
+            const fileType = uploadFile["type"];
+            const validImageType = ["image/jpeg", "image/jpg", "image/png"];
+
+            if (!validImageType.includes(fileType)) {
+                alert("Please insert only jpeg, jpg and png!");
+            } else {
+                if (uploadFile) {
+                    console.log(URL.createObjectURL(uploadFile));
+                    $(".upload-img-frame")
+                        .attr("src", URL.createObjectURL(uploadFile))
+                        .addClass("success");
+                }
+
+                filename = $(this)[0].files[0].name;
+                $(this).siblings(".upload-name").val(filename);
+            }
+        }
+    });
+
+});
 
 function validateSignupForm() {
     const memberNick = $('.member-nick').val();

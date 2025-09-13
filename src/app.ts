3973fb624fb14278dee172a55e85d535678d7449
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-09-13 12:03:42 +0900

    feat: integration of middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,14 @@ import routerAdmin from './router-Admin'
 import morgan from 'morgan'
 import { MORGAN_FORMAT } from './libs/config';
 
+import session from 'express-session';
+import ConnectMongoDb from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDb(session);
+const store = new MongoDBStore({
+    uri:String(process.env.MONGO_URL),
+    collection: "sessions"
+})
 
 //1 -- ENTERANCE --;
 const app = express();
@@ -15,7 +23,17 @@ app.use(morgan(MORGAN_FORMAT))
 
 
 // 2- sessions;
-
+app.use(
+    session({
+        secret:String(process.env.SESSION_SECRET),
+        cookie:{
+            maxAge:1000 * 3600 * 3, //3hours
+        },
+        store: store,
+        resave: true,
+        saveUninitialized:true
+    })
+);
 
 // 3- views
 app.set('views', path.join(__dirname, 'views'));

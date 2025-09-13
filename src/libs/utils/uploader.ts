ff536fbb32375c9aaf9028afdc77487da0ce2fda
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-09-14 01:55:03 +0900

    feat: developed create new product frontend logic

--- src/libs/utils/uploader.ts
+++ src/libs/utils/uploader.ts
@@ -1,12 +1,19 @@
 import path from "path";
 import multer from "multer";
 import { v4 } from "uuid";
+import fs from "fs";
 
 
 function getTargetImageStorage(address: any) {
     return multer.diskStorage({
       destination: function (req, file, cb) {
-        cb(null, `./uploads/${address}`);
+        const uploadPath = path.join(__dirname, "..", "..", "uploads", address);
+
+        // 📁 Papka mavjudligini tekshirish va yaratish
+        if (!fs.existsSync(uploadPath)) {
+          fs.mkdirSync(uploadPath, { recursive: true });
+        }
+        cb(null, uploadPath);
       },
       filename: function (req, file, cb) {
         const extension = path.parse(file.originalname).ext;

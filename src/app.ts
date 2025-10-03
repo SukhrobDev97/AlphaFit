b0a0bc802e16e42a50865f4e7cc9d71e4427f322
Sukhrob <zeboismailova@Sukhrob-MacBookPro.local>
2025-10-03 14:53:15 +0900

    feat: develop authentication via tokens with cookies

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { MORGAN_FORMAT } from './libs/config';
 import session from 'express-session';
 import ConnectMongoDb from "connect-mongodb-session";
 import { T } from './libs/types/common';
+import cookieParser from 'cookie-parser';
 
 const MongoDBStore = ConnectMongoDb(session);
 const store = new MongoDBStore({
@@ -23,6 +24,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(cookieParser())
 app.use(morgan(MORGAN_FORMAT))
 
 
